83f391799b6a6847f88b3cfffed1446c87e18961
thisiskingsley <crownking89@yahoo.com>
2024-11-02 17:30:25 -0500

    updated socket config

--- socket.ts
+++ socket.ts
@@ -3,4 +3,6 @@
 import { io } from 'socket.io-client';
 
 // export const socket = io('http://localhost:3001');
-export const socket = io('https://stellusrxpoker-de9a8cd9d661.herokuapp.com');
+export const socket = io('https://stellusrxpoker-de9a8cd9d661.herokuapp.com', {
+	transports: ['websocket', 'polling', 'flashsocket'],
+});
