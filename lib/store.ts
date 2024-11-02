92117770aa21085850a777ec3a4d2befbad9319f
thisiskingsley <crownking89@yahoo.com>
2024-11-02 15:58:01 -0500

    added export types

--- lib/store.ts
+++ lib/store.ts
@@ -42,4 +42,11 @@ export const store = configureStore({
 		}).concat(apiSlice.middleware),
 });
 
+// Get the type of our store variable
+export type AppStore = typeof store;
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<AppStore['getState']>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = AppStore['dispatch'];
+
 export const persistor = persistStore(store);
